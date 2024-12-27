052771a9cb2d36f90f2e63c9deab9379418fc8b9
MaksyDarko <darko.punktooy@gmail.com>
2024-12-27 19:48:07 +0100

    #changed file

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,16 @@ import { RouterProvider } from "react-router-dom";
 import "./index.css";
 import App from "./App.jsx";
 import routes from "./routes.jsx";
-import { QueryClientProvider , QueryClient} from "@tanstack/react-query";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
 const client = new QueryClient();
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <QueryClientProvider client={client}>
-    <RouterProvider router={routes}>
-      <App />
-    </RouterProvider>
+      <RouterProvider router={routes}>
+        <App />
+      </RouterProvider>
     </QueryClientProvider>
   </StrictMode>
 );
--- src/routes.jsx
+++ src/routes.jsx
@@ -4,27 +4,35 @@ import Products from "./pages/Products";
 import Layout from "./layout/Layout";
 import ShoppingCart from "./pages/ShoppingCart";
 import ErrorPage from "./layout/ErrorPage";
+import { Redirect } from "react-router";
 
-const routes = createBrowserRouter([
-  {
-    path: "/",
-    element: <Layout />,
-    errorElement: <ErrorPage />, 
-    children: [
-      {
-        path: "/",
-        element: <Dashboard />,
-      },
-      {
-        path: "products",
-        element: <Products />, 
-      },
-      {
-        path: "/cart",
-        element: <ShoppingCart />,
-      },
-    ],
-  },
-]);
+const routes = createBrowserRouter(
+  [
+    // {
+    //   path: "/",
+    //   element: <Redirect to="E-Shop" />,
+    // },
+    {
+      path: "/",
+      element: <Layout />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          path: "/",
+          element: <Dashboard />,
+        },
+        {
+          path: "products",
+          element: <Products />,
+        },
+        {
+          path: "/cart",
+          element: <ShoppingCart />,
+        },
+      ],
+    },
+  ],
+  { basename: "/E-Shop" }
+);
 
 export default routes;
